c0b4ed00b49019c6fa822cfe098005e46a202edb
jingle man <jingle_man@github.com>
2018-07-18 11:40:48 -0700

    cleaned that up

--- main.cpp
+++ main.cpp
@@ -12,54 +12,8 @@ using std::vector;
 using std::cout;
 using std::endl;
 
-template <class T>
-class C
-{
-  T _t;
-};
-
 int main()
 {
-
-  cout << endl << "float:";   // double basically same
-  cout << endl <<  numeric_limits<float>::has_infinity;
-  cout << endl << -numeric_limits<float>::infinity();
-  cout << endl <<  numeric_limits<float>::lowest();
-  cout << endl <<  numeric_limits<float>::min();
-  cout << endl <<  numeric_limits<float>::max();
-  cout << endl <<  numeric_limits<float>::infinity();
-  cout << endl;
-
-  cout << endl << "int:";
-  cout << endl <<  numeric_limits<int>::has_infinity;
-  cout << endl << -numeric_limits<int>::infinity();
-  cout << endl <<  numeric_limits<int>::lowest();
-  cout << endl <<  numeric_limits<int>::min();
-  cout << endl <<  numeric_limits<int>::max();
-  cout << endl <<  numeric_limits<int>::infinity();
-  cout << endl;
-
-  cout << endl << "size_t:";
-  cout << endl <<  numeric_limits<size_t>::has_infinity;
-  cout << endl << -numeric_limits<size_t>::infinity();
-  cout << endl <<  numeric_limits<size_t>::lowest();
-  cout << endl <<  numeric_limits<size_t>::min();
-  cout << endl <<  numeric_limits<size_t>::max();
-  cout << endl <<  numeric_limits<size_t>::infinity();
-  cout << endl;
-
-  cout << endl << "unsigned char:";   // double basically same
-  cout << endl <<  numeric_limits<unsigned char>::has_infinity;
-  cout << endl << -numeric_limits<unsigned char>::infinity();
-  cout << endl <<  numeric_limits<unsigned char>::lowest();
-  cout << endl <<  numeric_limits<unsigned char>::min();
-  cout << endl <<  numeric_limits<unsigned char>::max();
-  cout << endl <<  numeric_limits<unsigned char>::infinity();
-  cout << endl;
-
-  return 1;
-
-
   size_t n = 10;
   vector<float, std::allocator<float>> v;
   srand(time(0));
